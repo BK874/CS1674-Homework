47d8fca0e3ce35217829c0a874f05cf3388ad05f
Brian Knotten <bjk69@pitt.edu>
2018-01-23 23:48:44 -0500

    my unique

--- my_unique.m
+++ my_unique.m
@@ -5,5 +5,32 @@
 
 function [N] = my_unique(M)
 
-for i:size(M, 1)
+dupe = zeros(size(M,1), 1);
+
+for i = 1:size(M, 1)
     test_row = M(i, :);
+    for j = i:size(M, 1)
+        if j ~= i;
+            if M(j, :) == M(i, :)
+                for l = 1:size(dupe, 1)
+                    if l == 0
+                        dupe(l, 1) = j
+                    end
+                end
+            end
+        end
+    end
+end
+
+S = size(M, 1);
+
+for z = 1:S
+    if z ~= 0
+        M(z, :) = []
+        if size(M, 1) < S
+            break
+        end
+    end
+end
+
+N = M;
